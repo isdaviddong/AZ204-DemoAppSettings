4de0807fecb885cf965997f1d4729ec88bbad334
twdeveloper <twdeveloper@outlook.com>
2022-05-31 23:56:15 +0800

    v1

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using Microsoft.Extensions.Configuration;
+
+namespace webapp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        public string para1 = "";
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            para1 = config.GetSection("myConfig:para1").Value;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
